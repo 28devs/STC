61d353dc64f8257327e5ed66dbe8c3f3519abf33
Yuriy Krugoviy <CircularBoy@users.noreply.github.com>
2018-07-19 17:10:18 +0300

    services

---
+++ app/scripts/main.js
@@ -0,0 +1,33 @@
+//
+// About certs slider
+//
+
+const servicesSlider = document.querySelector('.services__slider .glide');
+
+if (servicesSlider) {
+  var servicesSliderInit = false;
+
+  const servicesSliderFn = function() {
+    if (window.innerWidth < 768) {
+      if (!servicesSliderInit) {
+        servicesSliderInit = new Glide(servicesSlider, {
+          perView: 1,
+          breakpoints: {
+            385: {
+              perView: 1
+            }
+          }
+        }).mount();
+      }
+    } else {
+      // destroy slider if init
+      if (typeof servicesSliderInit === 'object') {
+        servicesSliderInit.destroy();
+        servicesSliderInit = false;
+      }
+    }
+  };
+
+  servicesSliderFn();
+  window.addEventListener('resize', servicesSliderFn);
+}
